3b085512ac96173b3f284de2cae85c49bd612fdd
sujatha <jhanavi.bhushan@gmail.com>
2020-07-10 15:21:56 -0400

    updated backend routes to handle signup and login functionalities making requests to controller

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,20 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
-// Matches with "/api/books"
-router.route("/")
-  .get(userController.findAll)
-  .post(userController.create);
-// Matches with "/api/books/:id"
-// router
-//   .route("/:id")
-//   .get(bookController.findById)
-//   .put(bookController.update)
-//   .delete(bookController.remove);
+// // Matches with "/api/books"
+// router.route("/")
+//   .get(userController.findAll)
+//   .post(userController.create);
+// // Matches with "/api/books/:id"
+// // router
+// //   .route("/:id")
+// //   .get(bookController.findById)
+// //   .put(bookController.update)
+// //   .delete(bookController.remove);
+
+router.get('/',userController.findAll)
+
+router.post('/signup', userController.signup);
+
+router.post('/login', userController.login);
+
 module.exports = router;
