f0d62dcd3028bdb26d6a263cdbadf61816edca3c
sujatha <jhanavi.bhushan@gmail.com>
2020-07-10 15:17:50 -0400

    implemented backend controller to handle signup and login functionalities making requests to db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,176 @@
 const db = require("../models");
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
-// Defining methods for the bookController
-module.exports = {
-  findAll: function(req, res) {
-    db.User.find(req.query)
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
-  },
-  // findById: function(req, res) {
-  //   db.Book.findById(req.params.id)
-  //     .then(dbBook => res.json(dbBook))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  create: function(req, res) {
-    db.User.create(req.body)
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
-  },
-  // update: function(req, res) {
-  //   db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-  //     .then(dbBook => res.json(dbBook))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  // remove: function(req, res) {
-  //   db.Book.findById(req.params.id)
-  //     .then(dbBook => dbBook.remove())
-  //     .then(dbBook => res.json(dbBook))
-  //     .catch(err => res.status(422).json(err));
-  // }
+// // Defining methods for the bookController
+// module.exports = {
+//   // findById: function(req, res) {
+//   //   db.Book.findById(req.params.id)
+//   //     .then(dbBook => res.json(dbBook))
+//   //     .catch(err => res.status(422).json(err));
+//   // },
+//   // update: function(req, res) {
+//   //   db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+//   //     .then(dbBook => res.json(dbBook))
+//   //     .catch(err => res.status(422).json(err));
+//   // },
+//   // remove: function(req, res) {
+//   //   db.Book.findById(req.params.id)
+//   //     .then(dbBook => dbBook.remove())
+//   //     .then(dbBook => res.json(dbBook))
+//   //     .catch(err => res.status(422).json(err));
+//   // }
+// };
+
+const findAll = async (req, res, next) => {
+  db.User.find(req.query)
+    .then(dbUser => res.json(dbUser))
+    .catch(err => res.status(422).json(err));
+}
+
+const signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
+  }
+
+  const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  if (existingUser) {
+    const error = new HttpError(
+      'User exists already, please login instead.',
+      422
+    );
+    return next(error);
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      'Could not create user, please try again.',
+      500
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser) {
+    const error = new HttpError(
+      'Invalid user, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      'Could not log you in, please check your credentials and try again.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  res.json({
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token
+  });
+};
+
+exports.signup = signup;
+exports.login = login;
+exports.findAll = findAll;
