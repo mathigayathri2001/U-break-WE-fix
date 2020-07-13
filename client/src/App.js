d18c5b1078c28e5d85e6aa9d31f0484568fadbb4
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-07-13 18:45:43 -0400

    Added route for handyman and user login

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,58 @@
-import React, { useState, useCallback } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import SignUp from "./pages/signUp";
-import Login from "./pages/login";
-import HandySignup from "./pages/handysignUp";
-import Nav from "./components/Navbar";
-import { AuthContext } from './utils/auth-context';
+import React, { useState, useCallback } from 'react'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import SignUp from './pages/signUp'
+import Login from './pages/login'
+import HandySignup from './pages/handysignUp'
+import Nav from './components/Navbar'
+import { AuthContext } from './utils/auth-context'
+import Handylogin from './components/Handylogin'
+import Userlogin from './components/Userlogin'
+
 // import Searchbar from './components/Searchbar';
 // import Books from "./pages/Books";
 // import Nav from "./components/Nav";
-function App() {
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
-  const [handymanId, setHandymanId] = useState(false);
+
+function App () {
+  const [token, setToken] = useState(false)
+  const [userId, setUserId] = useState(false)
+  const [handymanId, setHandymanId] = useState(false)
 
   const login = useCallback((uid, token) => {
-    setToken(token);
-    setUserId(uid);
-    setHandymanId(uid);
-  }, []);
+    setToken(token)
+    setUserId(uid)
+    setHandymanId(uid)
+  }, [])
 
   const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-    setHandymanId(null);
-  }, []);
+    setToken(null)
+    setUserId(null)
+    setHandymanId(null)
+  }, [])
 
   return (
     <div>
       <AuthContext.Provider
-      value={{
-        isLoggedIn: !!token,
-        token: token,
-        userId: userId,
-        handymanId: handymanId,
-        login: login,
-        logout: logout
-      }}
-    >
+        value={{
+          isLoggedIn: !!token,
+          token: token,
+          userId: userId,
+          handymanId: handymanId,
+          login: login,
+          logout: logout
+        }}
+      >
         <Router>
-       {/* < SignUp /> */}
-         <Nav/>
-         {/* <Searchbar/> */}
-         <Route exact path="/signup" component={SignUp} />
-         <Route exact path="/login" component={Login} />
-         <Route exact path="/handysignUp" component={HandySignup} />
+          {/* < SignUp /> */}
+          <Nav />
+          {/* <Searchbar/> */}
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/handysignUp' component={HandySignup} />
+          <Route exact path='/handylogin' component={Handylogin} />
+          <Route exact path='/userlogin' component={Userlogin} />
         </Router>
-        </AuthContext.Provider>
+      </AuthContext.Provider>
     </div>
-  );
+  )
 }
-export default App;
+export default App
