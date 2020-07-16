b0ed3a93d63bd5ff1368e828e2db3e6365332187
sujatha <jhanavi.bhushan@gmail.com>
2020-07-16 13:36:20 -0400

    modified logout page

--- client/src/components/Logout/index.js
+++ client/src/components/Logout/index.js
@@ -7,10 +7,9 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 import { Link, Redirect } from "react-router-dom"
 import IconButton from "@material-ui/core/IconButton";
 import HomeIcon from '@material-ui/icons/Home';
-import Api from "../../utils/API"
-import FavoriteIcon from '@material-ui/icons/Favorite';
+// import Api from "../../utils/API"
+// import FavoriteIcon from '@material-ui/icons/Favorite';
 import Tooltip from '@material-ui/core/Tooltip';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 //styling 
 const useStyles = makeStyles((theme) => ({
@@ -47,25 +46,11 @@ export default function SearchAppBar() {
 
   const [redirect, setRedirect] = useState("")
 
-//   //Logout functionality
-//   const logout = () => {
-//     Api.logout().then(response => {
-//       console.log(response.data)
-//       if (response.status === 200) {
-//         setRedirect("/home")
-//       }
-//     }).catch(error => {
-//       console.log('Logout error')
-//       console.log(error)
-//     })
-//   }
-
   //If yu click logout, redirect the page, if not show the current page
   if (redirect) {
     return <Redirect to={{ pathname: redirect }} />
   } else {
     return (
-
       <div className={classes.root}>
         <AppBar position="static" className={classes.navBar}>
           <Toolbar>
@@ -78,9 +63,8 @@ export default function SearchAppBar() {
             <IconButton>
               <Tooltip title="View Request"><Link className={classes.links} to="/viewRequest" > viewRequest </Link></Tooltip>
             </IconButton>
-            {/* <Tooltip title="Log Out"><IconButton className={classes.links} onClick={logout}><ExitToAppIcon/></IconButton></Tooltip> */}
             <IconButton>
-            <Tooltip title="Log Out"><Link className={classes.links} to="/home">Logout</Link></Tooltip>
+            <Tooltip title="Log Out"><Link className={classes.links} to="/">Logout</Link></Tooltip>
             </IconButton>
           </Toolbar>
         </AppBar>
