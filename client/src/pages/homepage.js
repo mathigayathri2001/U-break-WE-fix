ee50961685fde3ba62da24616589599664ee4f9f
joshb4u <joshb4u@gmail.com>
2020-07-18 08:22:59 -0400

    updated homepage with carousel component

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from '../components/Navbar'
+import Carousel from '../components/Carousel/carousel.js'
 
 function Home() {
     return (
         <div>
             <Nav/>
+            <Carousel/>
         </div>
     )
 }
